3467e77eb56bc669a83ddd8ba6b6f53b4906d8b8
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2017-09-18 15:22:30 +0100

    Mallow transform working, still to correct case '

--- postman/postman2.py
+++ postman/postman2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # Exepctations can be gotten from https://www.upwork.com/jobs/~01c25b5935cf7e66bb
 
-# Declaring variables that will be used widely
 import sys
 
+# Declaring variables that will be used widely
 size = None
 mailboxes = []
 def main_menu() :
@@ -29,30 +29,40 @@ def main_menu() :
 #Solving the postman problem
 def handleSolver() :
     global size
+    global mailboxes
     size = initializeVars()
+    mailboxes = [item.lower() for item in mailboxes]
+    #print("initial mailbox is " + str(mailboxes))
+
     with open('output'+str(size)+'.txt',"a") as output:
         #n= 0
-        j = 1
+        j=1
         i = 2
-        for i in range(2,size) :
-            for j in range(1,int(size/j),i) :
-                if j < size-1 and mailboxes[j].lower() == 'c':
-                    mailboxes[j+1] = 'O'
-                elif j < size-1 and mailboxes[j].lower() == 'o':
-                    mailboxes[j+1] = 'C'
-                else:
-                    raise Exception('There is a problem with the content of the mailbox')
+        for i in range(2,size,1) :
+            print('i is %d and j is %d '% (i,j))
+            for j in range(i, size,1) :
+                print('i %d size %d int(size/i) %d, j %d'%(i,size, int(size/i),j))
+                if (j%i == 0) :
+                    if j < size and mailboxes[j+1].lower() == 'c':
+                        mailboxes[j+1] = 'O'
+                    elif j < size and mailboxes[j+1].lower() == 'o':
+                        mailboxes[j+1] = 'C'
+                    else:
+                        print(str(mailboxes[j]))
+                        raise Exception('There is a problem with the content of the mailbox')
+                #j+=i
                 output.write('\n'+str(mailboxes))
                 print(str(mailboxes))
-                #n += 1
-        #output.write('\n'+str(mailboxes))
+
+                mailboxes = [item.lower() for item in mailboxes]
+            print("loop is " +str(i))
 
 def initializeVars() :
     global mailboxes
     sze = 0
     while True:
         try:
-            sze = int(input ("Please enter the size of mailboxes :"))
+            sze = int(input ("Please enter the size of mailboxes: "))
             break
         except :
             print ('Please Enter a valid option')
@@ -61,9 +71,9 @@ def initializeVars() :
     with open('output'+str(sze)+'.txt',"w+") as output:
         output.write(str(mailboxes))
     print(str(mailboxes))
-    for i in range(0,sze,2):
+    for i in range(0,sze-1,2):
         if i % 2 == 0 :
-            mailboxes[i] = 'O'
+            mailboxes[i+1] = 'O'
     with open('output'+str(sze)+'.txt',"a") as output:
         output.write('\n'+str(mailboxes))
     print(str(mailboxes))
