791fe58283e046aad726f65384382e6d5bef9c93
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2017-09-22 02:27:26 +0100

    miles work functioning with parameters and necessary files (intput and output)

--- miles/organise_log.py
+++ miles/organise_log.py
@@ -2,27 +2,34 @@
 #This script goes through a set of logs and produces a resulting file containing some extracted data from the file.
 from itertools import islice
 import re
+import sys
+import argparse
 
+generated_line = None
+#dl = dh = cpl = cph = gl = gh  = None
 def filterfile (log_file, output_file) :
+    global generated_line
 
     searchString = "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
     previos_line = None
     with open(log_file) as log:
-        with open(output_file,"a+") as output:
+        with open(output_file,"w+") as output:
             output.write("%-35s   %-35s  %-35s  %-35s  %-20s\n" % ("Field","County","Depth","Cumm PROD","Gravity"))
             output.write('*'*165 +'\n')
             for line in log :
                 if line.strip().startswith(searchString):
                     line_before_slice = line
                     nextLine = ''.join(islice(log, 1))
-                    formed_line = extractData(previos_line,line_before_slice,nextLine)
-                    output.write(formed_line)
-                    print(formed_line)
+                    if(extractData(previos_line,line_before_slice,nextLine)):
+                        output.write(generated_line)
+                        print(generated_line)
                 previos_line = line
 
 
 
 def extractData(previous, current, next):
+    global generated_line
+
     #Extract cummulative production in the current line
     cumm_prod = current.split()[-1]
 
@@ -37,10 +44,56 @@ def extractData(previous, current, next):
     county = ' '.join(county_temp[0][::-1])
     field = previous.split(county[0].strip())[0]
 
+    generated_line = "%-35s   %-35s  %-35s  %-35s  %-20s\n" % (str(field).strip(), str(county).strip(), str(depth).strip(), str(cumm_prod).strip(), str(gravity).strip())
+
+    if((int(depth) >= dl) and (int(depth)<dh) and (int(cumm_prod) >= cpl) and (int(cumm_prod) < cph) and (int(gravity) >= gl) and (int(gravity) < gh)):
+        return True
+
+    return False
 
 
-    return "%-35s   %-35s  %-35s  %-35s  %-20s\n" % (str(field).strip(), str(county).strip(), str(depth).strip(), str(cumm_prod).strip(), str(gravity).strip())
-    return "good"
+if __name__ == "__main__":
+    global dl ; global dh ; global cpl ; global cpl ; global gl ; global gh
+    ##********************************* Parsing User input with options and arguments *********************************######
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(description='An application to extract data from files.')
 
+    # Required positional argument
+    parser.add_argument('-i', type=str, required=True, help='path and name of the input file to run')
+    parser.add_argument('-o', type=str, required=True, help='path and name of the output file to use')
+    parser.add_argument('-dl', type=int, default=0, help='path and name of the log file to use')
+    parser.add_argument('-dh', type=int, default=sys.maxsize, help='path and name of the log file to use')
+    parser.add_argument('-cpl', type=int, default=0, help='path and name of the log file to use')
+    parser.add_argument('-cph', type=int,default=sys.maxsize, help='path and name of the log file to use')
+    parser.add_argument('-gl', type=int, default=0, help='path and name of the error file to use')
+    parser.add_argument('-gh', type=int, default=sys.maxsize,help='path and name of the error file to use')
 
-if __name__ == "__main__": filterfile("oilsch_1.txt","output.txt")
+    args = parser.parse_args()
+    if args.i is None :
+        print ('Please provide the path and name of the input file with logs , -- help for more info')
+        sys.exit()
+
+    if args.o is None :
+        print ('Please provide the path and name of the output file to use , -- help for more info')
+        sys.exit()
+
+    """
+    if args.f is None :
+        print ('Please provide the path and name of the syslog file to use , -- help for more info')
+        sys.exit()
+
+    if args.e is None :
+        print ('Please provide the path and name of the error file to use , -- help for more info')
+        sys.exit()
+    """
+
+    inputlog_file = args.i
+    output_file = args.o
+    dl = args.dl
+    dh = args.dh
+    cpl = args.cpl
+    cph = args.cph
+    gl = args.gl
+    gh = args.gh
+
+    filterfile(inputlog_file,output_file)
